87de7792fde5ee0c22687ba5d284d99b6fbaf9a7
Robin BIJAUDY <robin.bijaudy@gmail.com>
2020-12-03 11:54:25 +0100

    création entity aisShipType

---
+++ src/Entity/AisShipType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AisShipTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AisShipTypeRepository::class)
+ */
+class AisShipType
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="integer")
+     *@Assert\Length(min=1,
+     *           max=9,
+     *           minMessage = "Le type d'un navire est compris entre 1 et 9",
+     *           maxMessage = "Le type d'un navire est compris entre 1 et 9",
+     *           allowEmptyString = false
+     *           )
+     */
+    private $aisShipType;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInt(): ?int
+    {
+        return $this->int;
+    }
+
+    public function setInt(int $int): self
+    {
+        $this->int = $int;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getAisShipType(): ?string
+    {
+        return $this->aisShipType;
+    }
+
+    public function setAisShipType(string $aisShipType): self
+    {
+        $this->aisShipType = $aisShipType;
+
+        return $this;
+    }
+}
