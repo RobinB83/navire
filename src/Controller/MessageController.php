ef2059cd22b581e8ec4f9fbbcb7edceaeec7109b
Robin BIJAUDY <robin.bijaudy@gmail.com>
2020-12-09 14:11:26 +0100

    navbar and footer

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -5,18 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Message;
+use App\Form\MessageType;
 /**
  * @Route("/message", name="message_")
  */
 class MessageController extends AbstractController {
 
     /**
-     * @Route("/message", name="message")
+     * @Route("/contact", name="message")
      */
-    public function index(): Response {
+    public function demandeContact(Request $request) {
+        $contact = new Message();
+        $form = $this->createForm(MessageType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->addFlash('notification', "Votre message a bien été envoyé");
+            $contact = $form->getData();
+            $contact->setDatePremierContact(new \DateTime());
+
+            //$gestionContact->envoiMailContact($contact);
+
+
+            return $this->redirectToRoute("message");
+        }
         return $this->render('message/contact.html.twig', [
-                    'controller_name' => 'MessageController',
+                    'form' => $form->createView(),
         ]);
     }
 
